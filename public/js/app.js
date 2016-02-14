6d34ee98ed9c1a1051a4b5f9b00c7db31de9a174
Nikhil Verma <nikhilnxvverma1@gmail.com>
2016-02-13 23:00:09 -0500

    Integrated angular js and laid out item list on the results page.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,16 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+
+var app = angular.module('resultsApp', []);
+
+app.controller('ResultsController', [
+'$scope',
+function($scope){
+  $scope.test = 'TODO';
+  $scope.items = [
+    {title: 'post 1', artist:'artist',album:'album', duration: 873},
+    {title: 'post 2', artist:'artist',album:'album', duration: 873},
+    {title: 'post 3', artist:'artist',album:'album', duration: 873},
+    {title: 'post 4', artist:'artist',album:'album', duration: 873},
+    {title: 'post 5', artist:'artist',album:'album', duration: 873},
+    {title: 'post 6', artist:'artist',album:'album', duration: 873},
+  ];
+}]);
---
+++ public/js/controllers/ResultsController.js
@@ -0,0 +1,3 @@
+angular.module('resultsApp', []).controller('ResultsController', function($scope) {
+	$scope.test = 'Nothing beats a pocket protector!';
+});
