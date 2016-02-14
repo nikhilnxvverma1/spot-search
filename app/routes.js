088257885987ebdc76dd0a2d242d608d28a7425c
Nikhil Verma <nikhilnxvverma1@gmail.com>
2016-02-14 02:41:27 -0500

    Basic backend completed for the search request hit

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
 //	app.get('*', function(req, res) {
 //		res.sendfile('./public/index.html');
 //	});
-    
+
     app.get('/something', function(req, res) {
         console.log("Modified")
         res.send('Hitting the spotify api');
@@ -19,11 +19,28 @@ module.exports = function(app) {
                 console.log(body) // Print the google web page.
             }
         })
-        
+
 	});
-    
+
     app.get('/search', function(req, res) {
-	   res.sendfile('./public/results.html');
+
+      var url=require('url')
+      var urlParts=url.parse(req.url,true)
+      var query=urlParts.query;
+      var itemToSearch=query.myquery;
+      var request = require('request');
+      var endpoint="https://api.spotify.com/v1/search?q="+itemToSearch+"&type=track,artist,album&market=US";
+      request(endpoint, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+
+              console.log("Recieved response") // Print the google web page.
+              res.render('results',{
+                itemToSearch:itemToSearch,
+                result:body
+              });
+          }
+      })
+      // console.log("query is "+query.myquery);
 	});
 
 };
