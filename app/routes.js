480eddd11c57a95395a35039cf2ff3e0dab718fd
Nikhil Verma <nikhilnxvverma1@gmail.com>
2016-02-13 19:03:59 -0500

    Basic spotify search hit(backend)

--- app/routes.js
+++ app/routes.js
@@ -6,8 +6,20 @@ module.exports = function(app) {
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
-	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html');
+//	app.get('*', function(req, res) {
+//		res.sendfile('./public/index.html');
+//	});
+    
+    app.get('/something', function(req, res) {
+        console.log("Modified")
+        res.send('Hitting the spotify api');
+        var request = require('request');
+        request('https://api.spotify.com/v1/tracks/0eGsygTp906u18L0Oimnem', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body) // Print the google web page.
+            }
+        })
+        
 	});
 
 };
