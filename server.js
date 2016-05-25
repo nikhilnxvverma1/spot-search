6765a35c0e162dc600e5459de67de70bb1286168
Nikhil Verma <nikhilnxvverma1@gmail.com>
2016-05-25 11:48:00 -0400

    Added flight plan to it for deploying in production on droplet, changed port .

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var methodOverride = require('method-override');
 // config files
 var db = require('./config/db');
 
-var port = process.env.PORT || 8080; // set our port
+// var port = process.env.PORT || 8080; // set our port
+var port = 2223;
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
 // get all data/stuff of the body (POST) parameters
